5c6db0a4130e608b78a39901b0e0cee27c8aa4ab
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-16 15:34:59 -0400

    tests to see if a user can submit the form data

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ Set up tests that will...
  X - Get the Name input and type a name in it.
  X - Use an assertion to check if the text inputted contains the name you provided (Hint: use the .should assertion)
  X - Get the Email input and type an email address in it
- Get the password input and type a password in it
+ X - Get the password input and type a password in it [SUBSTITUTED TEST OF TEXTAREA BOX]
  X - Set up a test that will check to see if a user can check the terms of service box
- Check to see if a user can submit the form data
+ X Check to see if a user can submit the form data
  Check for form validation if an input is left empty
--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -4,19 +4,21 @@ describe('This is our first test!', () => {
     })
 })
 
-describe('testing our form inputs', () => {
+describe('testing our inputs', () => {
     beforeEach(() => {
         cy.visit('localhost:3000');
     })
 
     it('should hold the value of the typed name', () => {
-
         cy.get('input[name="name"]').type("Gabriel Shapiro").should("have.value", "Gabriel Shapiro");
-
     })
 
     it('it should hold the value of the typed email', () => {
-        cy.get('input[name="email"]').type("kafka@doge.com").should("have.value",  "kafka@doge.com");
+        cy.get('input[name="email"]').type("kafka@doge.com").should("have.value", "kafka@doge.com");
+    })
+
+    it('it should hold the value of the typed help request', () => {
+        cy.get('textarea[name="motivation"]').type("I need help writing tests").should("have.value", "I need help writing tests");
     })
 
     it('should allow user to check the checkbox', () => {
@@ -24,5 +26,24 @@ describe('testing our form inputs', () => {
     })
 })
 
+describe('tests submit button', () => {
 
+    it('should allow submit button to be clicked if all fields are filled & ToS box is checked', () => {
+
+        //visit the site
+        cy.visit('localhost:3000');
+
+        //fill out all fields and check the ToS box
+        cy.get('input[name="name"]').type("Gabriel Shapiro");
+        cy.get('input[name="email"]').type("kafka@doge.com");
+        cy.get('textarea[name="motivation"]').type("I need help writing tests");
+        cy.get('input[type="checkbox"]').check();
+
+        //click the submit button
+        cy.get('button').click();
+
+        //make sure some info from this user is now displayed
+        cy.get('pre').should('contain', 'Gabriel');
+    })
+})
 
