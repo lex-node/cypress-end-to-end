f01b6f685be0250ebaef3555d3b68dc59af62928
triggering-event <gabriel.j.shapiro@gmail.com>
2020-05-16 15:01:17 -0400

    adds test for inputting email

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Set up tests that will...
 
  X - Get the Name input and type a name in it.
  X - Use an assertion to check if the text inputted contains the name you provided (Hint: use the .should assertion)
- Get the Email input and type an email address in it
+ X - Get the Email input and type an email address in it
  Get the password input and type a password in it
  X - Set up a test that will check to see if a user can check the terms of service box
  Check to see if a user can submit the form data
--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,24 +1,25 @@
 describe('This is our first test!', () => {
-    it('should return true', ()=>{
+    it('should return true', () => {
         expect(true).to.equal(true);
     })
 })
 
-describe('testing our form inputs', ()=>{
-   beforeEach( ()=> {
+describe('testing our form inputs', () => {
+    beforeEach(() => {
         cy.visit('localhost:3000');
     })
 
-    it("finds the Name Input", ()=> {
-        cy.contains('Name')
-})
-
-    it('inputs name into the name input', ()=>{
+    it('should hold the value of the typed name', () => {
 
         cy.get('input[name="name"]').type("Gabriel Shapiro").should("have.value", "Gabriel Shapiro");
+
     })
 
-    it('checks that checkbox can be checked', ()=>{
+    it('it should hold the value of the typed email', () => {
+        cy.get('input[name="email"]').type("kafka@doge.com").should("have.value",  "kafka@doge.com");
+    })
+
+    it('should allow user to check the checkbox', () => {
         cy.get('input[type="checkbox"]').check().should("be.checked");
     })
 })
